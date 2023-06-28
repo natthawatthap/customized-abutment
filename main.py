ba0f3aff6a0be3ef4dad908e16ac7323304764d2
Osacn natthawat Thapayapattanakul <osacn_natthawat.thapayapattanakul@krungthai.com>
2023-06-28 16:55:35 +0700

    add ui

--- main.py
+++ main.py
@@ -2,38 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from stl import mesh
+import tkinter as tk
+from tkinter import filedialog
 
-# Load the first STL file
-stl_file1 = 'models/model1.stl'
-mesh_data1 = mesh.Mesh.from_file(stl_file1)
+# Function to handle button click event
+def load_stl_files():
+    # Open file dialog to select the STL files
+    stl_file_paths = filedialog.askopenfilenames(filetypes=[("STL Files", "*.stl")])
 
-# Load the second STL file
-stl_file2 = 'models/model2.stl'
-mesh_data2 = mesh.Mesh.from_file(stl_file2)
+    # Create a new figure and axes
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-# Merge the vectors and concatenate the points
-combined_vectors = np.concatenate([mesh_data1.vectors, mesh_data2.vectors])
-combined_points = np.concatenate([mesh_data1.points, mesh_data2.points])
+    for stl_file_path in stl_file_paths:
+        # Load the STL file
+        mesh_data = mesh.Mesh.from_file(stl_file_path)
 
-# Create a new figure and axes
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+        # Plot the surface
+        ax.add_collection3d(mplot3d.art3d.Poly3DCollection(mesh_data.vectors))
 
-# Plot the combined surface
-ax.add_collection3d(mplot3d.art3d.Poly3DCollection(combined_vectors))
+        # Set the aspect ratio of the plot
+        scale = mesh_data.points.flatten()
+        ax.auto_scale_xyz(scale, scale, scale)
 
-# Set the aspect ratio of the plot
-scale = combined_points.flatten()
-ax.auto_scale_xyz(scale, scale, scale)
+    # Function to handle zoom event
+    def on_scroll(event):
+        ax.set_xlim3d(ax.get_xlim3d())
+        ax.set_ylim3d(ax.get_ylim3d())
+        ax.set_zlim3d(ax.get_zlim3d())
 
-# Function to handle zoom event
-def on_scroll(event):
-    ax.set_xlim3d(ax.get_xlim3d())
-    ax.set_ylim3d(ax.get_ylim3d())
-    ax.set_zlim3d(ax.get_zlim3d())
+    # Connect the scroll event to the zoom function
+    fig.canvas.mpl_connect('scroll_event', on_scroll)
 
-# Connect the scroll event to the zoom function
-fig.canvas.mpl_connect('scroll_event', on_scroll)
+    # Show the plot
+    plt.show()
 
-# Show the plot
-plt.show()
+# Create the main window
+root = tk.Tk()
+
+# Create a button to load STL files
+load_button = tk.Button(root, text="Load STL Files", command=load_stl_files)
+load_button.pack()
+
+# Start the main event loop
+root.mainloop()
