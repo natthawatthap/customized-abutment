a3e5dc72c5b16426c83ec3ece24685093a5197e8
Osacn natthawat Thapayapattanakul <osacn_natthawat.thapayapattanakul@krungthai.com>
2023-06-28 22:48:13 +0700

    add note

---
+++ mesh_scan.py
@@ -0,0 +1,16 @@
+from stl import mesh
+import numpy as np
+
+# Load the STL file
+stl_file = 'models/model2.stl'
+mesh_data = mesh.Mesh.from_file(stl_file)
+
+# Get the vertices and faces
+vertices = mesh_data.vectors.reshape((-1, 3))
+faces = np.arange(len(vertices)).reshape((-1, 3))
+
+# Save vertices and faces to a text file
+output_file = 'model2.txt'
+np.savetxt(output_file, vertices, fmt='%f', delimiter=' ', header='Vertices', comments='')
+with open(output_file, 'a') as file:
+    np.savetxt(file, faces, fmt='%d', delimiter=' ', header='Faces', comments='')
