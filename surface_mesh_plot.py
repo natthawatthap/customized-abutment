2f30f69edc57e128a2b2994e2658149da47b4ea4
Osacn natthawat Thapayapattanakul <osacn_natthawat.thapayapattanakul@krungthai.com>
2023-06-29 03:24:37 +0700

    fix plot

--- surface_mesh_plot.py
+++ surface_mesh_plot.py
@@ -4,10 +4,10 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from stl import mesh
 from matplotlib.widgets import Slider
-from matplotlib import cm
+import matplotlib.cm as cm
 
 # Load the STL file
-your_mesh = mesh.Mesh.from_file('models/model2.stl')
+your_mesh = mesh.Mesh.from_file('model.stl')
 
 # Extract vertex coordinates from the mesh
 vertices = your_mesh.vectors.reshape(-1, 3)
@@ -23,58 +23,70 @@ normalized_heights = (z_coords - z_min) / (z_max - z_min)
 # Create colormap
 cmap = cm.get_cmap('rainbow')
 
-# Create subplots for each plane
-fig = plt.figure(figsize=(16, 8))
+# Create subplots for each view
+fig = plt.figure(figsize=(16, 12))
 
-# XY plane
-ax_xy = fig.add_subplot(231, projection='3d')
-ax_xy.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
-ax_xy.set_xlabel('X')
-ax_xy.set_ylabel('Y')
-ax_xy.set_zlabel('Z')
-ax_xy.set_title('XY Plane')
+# Top view
+ax_top = fig.add_subplot(231, projection='3d')
+ax_top.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
+ax_top.set_xlabel('X')
+ax_top.set_ylabel('Y')
+ax_top.set_zlabel('Z')
+ax_top.set_title('Top View')
+ax_top.view_init(elev=90, azim=-90)
 
-# YZ plane
-ax_yz = fig.add_subplot(232, projection='3d')
-ax_yz.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
-ax_yz.set_xlabel('Y')
-ax_yz.set_ylabel('Z')
-ax_yz.set_zlabel('X')
-ax_yz.set_title('YZ Plane')
+# Bottom view
+ax_bottom = fig.add_subplot(232, projection='3d')
+ax_bottom.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
+ax_bottom.set_xlabel('X')
+ax_bottom.set_ylabel('Y')
+ax_bottom.set_zlabel('Z')
+ax_bottom.set_title('Bottom View')
+ax_bottom.view_init(elev=-90, azim=-90)
 
-# XZ plane
-ax_xz = fig.add_subplot(233, projection='3d')
-ax_xz.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
-ax_xz.set_xlabel('X')
-ax_xz.set_ylabel('Z')
-ax_xz.set_zlabel('Y')
-ax_xz.set_title('XZ Plane')
+# Left view
+ax_left = fig.add_subplot(233, projection='3d')
+ax_left.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
+ax_left.set_xlabel('X')
+ax_left.set_ylabel('Y')
+ax_left.set_zlabel('Z')
+ax_left.set_title('Left View')
+ax_left.view_init(elev=0, azim=180)
 
-# Upper plane
-ax_upper = fig.add_subplot(234, projection='3d')
-ax_upper.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
-ax_upper.set_xlabel('X')
-ax_upper.set_ylabel('Y')
-ax_upper.set_zlabel('Z')
-ax_upper.set_title('Upper Plane')
-ax_upper.view_init(elev=90, azim=-90)  # Adjust view angle
+# Right view
+ax_right = fig.add_subplot(234, projection='3d')
+ax_right.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
+ax_right.set_xlabel('X')
+ax_right.set_ylabel('Y')
+ax_right.set_zlabel('Z')
+ax_right.set_title('Right View')
+ax_right.view_init(elev=0, azim=0)
 
-# Lower plane
-ax_lower = fig.add_subplot(235, projection='3d')
-ax_lower.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
-ax_lower.set_xlabel('X')
-ax_lower.set_ylabel('Y')
-ax_lower.set_zlabel('Z')
-ax_lower.set_title('Lower Plane')
-ax_lower.view_init(elev=-90, azim=-90)  # Adjust view angle
+# Front view
+ax_front = fig.add_subplot(235, projection='3d')
+ax_front.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
+ax_front.set_xlabel('X')
+ax_front.set_ylabel('Y')
+ax_front.set_zlabel('Z')
+ax_front.set_title('Front View')
+ax_front.view_init(elev=0, azim=-90)
+
+# Back view
+ax_back = fig.add_subplot(236, projection='3d')
+ax_back.add_collection3d(Poly3DCollection(your_mesh.vectors, facecolors=cmap(normalized_heights)))
+ax_back.set_xlabel('X')
+ax_back.set_ylabel('Y')
+ax_back.set_zlabel('Z')
+ax_back.set_title('Back View')
+ax_back.view_init(elev=0, azim=90)
 
 # Create sliders for graph scale control
-scale_slider = Slider(fig.add_axes([0.2, 0.1, 0.6, 0.03]), 'Scale', 0.1, 20.0, valinit=1.0)
+scale_slider = Slider(fig.add_axes([0.2, 0.05, 0.6, 0.03]), 'Scale', 0.1, 20.0, valinit=1.0)
 
 # Function to update graph scale
 def update_scale(val):
     scale = scale_slider.val
-    for ax in [ax_xy, ax_yz, ax_xz, ax_upper, ax_lower]:
+    for ax in [ax_top, ax_bottom, ax_left, ax_right, ax_front, ax_back]:
         ax.cla()  # Clear the axes
         ax.set_xlim3d(-scale, scale)
         ax.set_ylim3d(-scale, scale)
